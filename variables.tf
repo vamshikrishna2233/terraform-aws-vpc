1599ae6fe16c0a1be77acca7c2d5d38ddd80b070
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-09-14 11:12:33 -0400

    terraform

---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "cidr_block" {
+  #default = "10.0.0.0/0"
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "common_tags" {
+  default = {} # this means tags is optional
+  type = map
+}
+
+variable "vpc_tags" {
+  default = {} # this means tags is optional
+  type = map
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidr" {
+  
+}
+
+variable "public_subnet_names" {
+  
+}
+
+variable "azs" {
+  
+}
+
+variable "private_subnet_cidr" {
+  
+}
+
+variable "private_subnet_names" {
+  
+}
+
+variable "database_subnet_cidr" {
+  
+}
+
+variable "database_subnet_names" {
+  
+}
+
+# variable "public_route_table_tags" {
+#   default = {}
+# }
+
+# variable "private_route_table_tags" {
+  
+# }
+
+# variable "database_route_table_tags" {
+  
+# }
